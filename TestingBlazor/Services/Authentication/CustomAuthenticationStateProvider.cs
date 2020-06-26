e116b03d1b97c0322df9c83890397384b6619b7d
Públio Martins Romano Montebello Carreiro <publioromano@hotmail.com>
2020-06-26 16:07:52 +0100

    Login, redirect, autentication, componentes and DI

---
+++ TestingBlazor/Services/Authentication/CustomAuthenticationStateProvider.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Components.Authorization;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace TestingBlazor.Services.Authentication
+{
+	public class CustomAuthenticationStateProvider : AuthenticationStateProvider
+	{
+		private string username;
+		private string password;
+
+		public void LoadUser(string username, string password)
+		{
+			this.username = username;
+			this.password = password;
+		}
+
+		public async Task<AuthenticationState> Logout()
+		{
+			Clear();
+			return new AuthenticationState(new ClaimsPrincipal(new ClaimsIdentity()));
+		}
+
+		public async override Task<AuthenticationState> GetAuthenticationStateAsync()
+		{
+
+			if (this.username == "teste@teste.com" && this.password == "123")
+			{
+				var identity = new ClaimsIdentity(
+					new[] {
+					new Claim(ClaimTypes.Name, this.username),
+					}, "Own authentication Type");
+
+				Clear();
+
+				var authState = new AuthenticationState(new ClaimsPrincipal(identity));
+				base.NotifyAuthenticationStateChanged(Task.FromResult(authState));
+				return authState;
+			}
+			else
+			{
+				Clear();
+				return new AuthenticationState(new ClaimsPrincipal(new ClaimsIdentity()));
+			}
+
+		}
+
+		private void Clear()
+		{
+			this.username = null;
+			this.password = null;
+		}
+	}
+}
